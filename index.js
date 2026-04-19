3dc65ab1bf00605eba63aff6e4cf63ce4d5edab0
Soham Vibhandik <sohamsonar71@gmail.com>
2026-04-20 00:06:33 +0530

    handled database password

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 import express from "express";
 import mysql from "mysql2";
+import dotenv from "dotenv";
+dotenv.config();
+
 const app = express();
 const port = 3000;
 
@@ -217,10 +220,10 @@ app.listen(port,()=>{
 //Database
 
 const db = mysql.createConnection({
-    host : "localhost",
-    user : "root",
-    password : "soham",
-    database : "blog_app"
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
 
 
