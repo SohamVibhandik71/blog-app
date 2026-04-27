c9d82da0795af04a9a7005cf4f3efb86cd065143
Soham Vibhandik <sohamsonar71@gmail.com>
2026-04-27 15:10:47 +0530

    implemented database triggers for logging blog CRUD operations

--- index.js
+++ index.js
@@ -245,7 +245,7 @@ app.post("/blogs", (req, res) => {
 });
 
 
-// ================== SERVER ==================
+//SERVER 
 
 app.listen(port, () => {
     console.log(`Server is running on port ${3000}.`);
--- mySQL queries/blog_app.sql
+++ mySQL queries/blog_app.sql
@@ -2,6 +2,8 @@ CREATE database blog_app;
 
 USE blog_app;
 
+//blogs(id, title, content, user_id) , users(id, name), comments(id,blog_id,user_id,content)
+
 
 CREATE TABLE users(
 	id INT AUTO_INCREMENT PRIMARY KEY,
@@ -22,7 +24,8 @@ CREATE TABLE comments(
     user_id INT,
     content TEXT,
     FOREIGN KEY (blog_id) REFERENCES blogs(id),
-    FOREIGN KEY (user_id) REFERENCES users(id)
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    Curr_date TIMESTAMP;
 );
 
 INSERT INTO users (name) VALUES 
@@ -37,3 +40,65 @@ INSERT INTO blogs (title, content, user_id) VALUES
 SELECT * FROM users;
 SELECT * FROM blogs;
 
+
+//triggers
+
+CREATE TABLE logs (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    action VARCHAR(50),
+    description TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
+DELIMITER $$
+CREATE TRIGGER after_blog_insert
+AFTER INSERT ON blogs
+FOR EACH ROW
+BEGIN
+    INSERT INTO logs (action, description)
+    VALUES (
+        'CREATE_BLOG',
+        CONCAT('New blog created with id ', NEW.id, ' by user ', NEW.user_id)
+    );
+END $$
+DELIMITER ;
+
+DELIMITER $$
+CREATE TRIGGER after_blog_delete
+AFTER DELETE ON blogs
+FOR EACH ROW
+BEGIN
+    INSERT INTO logs (action, description)
+    VALUES (
+        'DELETE_BLOG',
+        CONCAT('Blog deleted with id ', OLD.id)
+    );
+END $$
+DELIMITER ;
+
+DELIMITER $$
+CREATE TRIGGER after_comment_insert
+AFTER INSERT ON comments
+FOR EACH ROW
+BEGIN
+    INSERT INTO logs (action, description)
+    VALUES (
+        'ADD_COMMENT',
+        CONCAT('Comment added on blog ', NEW.blog_id, ' by user ', NEW.user_id)
+    );
+END $$
+DELIMITER ;
+
+DELIMITER $$
+CREATE TRIGGER after_blog_update
+AFTER UPDATE ON blogs
+FOR EACH ROW
+BEGIN
+    INSERT INTO logs (action, description)
+    VALUES (
+        'UPDATE_BLOG',
+        CONCAT('Blog with ID ', NEW.id, ' was edited')
+    );
+END $$
+DELIMITER ;
